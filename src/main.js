f276fb41f75dc8aaac0f1b9f8178c19dd1b193c0
Mutou <839608583@qq.com>
2021-12-05 21:26:43 +0800

    fest: add windicss  naiveUi

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+// windicss
+import 'virtual:windicss.css'
+// vfonts
+import 'vfonts/Lato.css'
+import 'vfonts/FiraCode.css'
 
 createApp(App).mount('#app')
